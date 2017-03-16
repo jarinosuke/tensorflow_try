2b9e80da9e49617bbb26f5e46c5479c11b85345a
Naoki Ishikawa <naokiishikawa0808@gmail.com>
2017-03-16 09:46:17 +0900

    added script that configures data

---
+++ my_own_scripts/split_data.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd # to load csv super easily
+
+df = pd.read_csv("../dataset/voice.csv", header=0)
+
+labels = (df["label"] == "male").values * 1
+labels = labels.reshape(-1, 1)
+
+del df["label"]
+data = df.values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(data, labels,
+                                               test_size=0.3, random_state=123456)
+
+np.save("X_train.npy", X_train) 
+np.save("X_test.npy", X_test)
+np.save("y_train.npy", y_train)
+np.save("y_test.npy", y_test)
