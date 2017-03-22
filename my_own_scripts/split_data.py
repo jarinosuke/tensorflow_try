178bfa1298d54fe461f96c679e61698f4155f1a4
Naoki Ishikawa <naokiishikawa0808@gmail.com>
2017-03-23 08:52:11 +0900

    moved tmp files

--- my_own_scripts/split_data.py
+++ my_own_scripts/split_data.py
@@ -13,7 +13,8 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(data, labels,
                                                test_size=0.3, random_state=123456)
 
-np.save("X_train.npy", X_train) 
-np.save("X_test.npy", X_test)
-np.save("y_train.npy", y_train)
-np.save("y_test.npy", y_test)
+tmp_dir = "../tmp/"
+np.save(tmp_dir + "X_train.npy", X_train) 
+np.save(tmp_dir + "X_test.npy", X_test)
+np.save(tmp_dir + "y_train.npy", y_train)
+np.save(tmp_dir + "y_test.npy", y_test)
