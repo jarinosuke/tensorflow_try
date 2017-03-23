f23e31de1f7d6e9a2c4ff45e37959d341777c4b1
Naoki Ishikawa <naokiishikawa0808@gmail.com>
2017-03-23 09:11:22 +0900

    implemented training

--- my_own_scripts/train.py
+++ my_own_scripts/train.py
@@ -1,11 +1,18 @@
+import os
 import numpy as np
 import tensorflow as tf
 
-X_train = np.load("X_train.npy")
-y_train = np.load("y_train.npy")
+checkpoint_dir = "../tmp/"
+print_every = 1000
+save_every = 10000
+
+X_train = np.load(checkpoint_dir + "X_train.npy")
+y_train = np.load(checkpoint_dir + "y_train.npy")
 
 num_inputs = 20
 num_classes = 1
+
+# setup computational graph
 
 with tf.name_scope("inputs"):
     x = tf.placeholder(tf.float32, [None, num_inputs], name="x-input")
@@ -35,3 +42,43 @@ with tf.name_scope("score"):
 
 with tf.name_scope("inference"):
     inference = tf.to_float(y_pred > 0.5, name="inference")
+
+# training classifier
+
+init = tf.global_variables_initializer()
+
+# For writing training checkpoints and reading them back in.
+saver = tf.train.Saver()
+tf.gfile.MakeDirs(checkpoint_dir)
+
+with tf.Session() as sess:
+    tf.train.write_graph(sess.graph_def, checkpoint_dir, "graph.rb", False)
+
+    sess.run(init)
+
+    # Sanity check: the initial loss should be 0.693146, which is -ln(0.5).
+    loss_value = sess.run(loss, feed_dict={x: X_train, y: y_train, regularization: 0})
+    print("Initial loss:", loss_value)
+
+    step = 0
+    while True:
+        perm = np.arange(len(X_train))
+        np.random.shuffle(perm)
+        X_train = X_train[perm]
+        y_train = y_train[perm]
+
+        feed = {x: X_train, y: y_train, learning_rate: 1e-2, 
+                                regularization: 1e-5}
+        sess.run(train_op, feed_dict=feed)
+
+        if step % print_every == 0:
+           train_accuracy, loss_value = sess.run([accuracy, loss], feed_dict=feed)
+           print("step: %4d, loss: %.4f, training accuracy: %.4f" % 
+                   (step, loss_value, train_accuracy))
+        
+        step += 1
+
+        if step % save_every == 0:
+            checkpoint_file = os.path.join(checkpoint_dir, "model")
+            saver.save(sess, checkpoint_file) 
+            print("*** SAVED MODEL ***")
