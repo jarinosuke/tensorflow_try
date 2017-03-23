bf6a9724d96ea53fb4a03f750365d49524f6f10e
Naoki Ishikawa <naokiishikawa0808@gmail.com>
2017-03-23 09:36:32 +0900

    implemented test of classifier accurancy

---
+++ my_own_scripts/test.py
@@ -0,0 +1,41 @@
+import os
+import numpy as np
+import tensorflow as tf
+from sklearn import metrics
+
+checkpoint_dir = "../tmp/"
+
+# Load the test data.
+X_test = np.load(checkpoint_dir + "X_test.npy")
+y_test = np.load(checkpoint_dir + "y_test.npy")
+
+#print("Test size:", X_test.shape)
+
+with tf.Session() as sess:
+    graph_file = os.path.join(checkpoint_dir, "graph.pb")
+    with tf.gfile.FastGFile(graph_file, "rb") as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+        tf.import_graph_def(graph_def, name="")
+
+        #print(graph_def.ListFields())
+
+        W = sess.graph.get_tensor_by_name("model/W:0")
+        b = sess.graph.get_tensor_by_name("model/b:0")
+
+        checkpoint_file = os.path.join(checkpoint_dir, "model")
+        saver = tf.train.Saver([W, b])
+        saver.restore(sess, checkpoint_file)
+
+        x = sess.graph.get_tensor_by_name("inputs/x-input:0")
+        y = sess.graph.get_tensor_by_name("inputs/y-input:0")
+        accuracy = sess.graph.get_tensor_by_name("score/accuracy:0")
+        print("Test set accuracy:", sess.run(accuracy, feed_dict={x: X_test, y: y_test}))
+
+        inference = sess.graph.get_tensor_by_name("inference/inference:0")
+        predictions = sess.run(inference, feed_dict={x: X_test})
+        print("\nClassification report:")
+        print(metrics.classification_report(y_test.ravel(), predictions))
+        print("Confusion matrix:")
+        print(metrics.confusion_matrix(y_test.ravel(), predictions))
+
